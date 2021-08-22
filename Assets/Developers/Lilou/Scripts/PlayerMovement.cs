0d6c83e610320f9a3ef73d07fd0c5e43e448fb34
Lilou Haefner <lhaefnerpersonal@gmail.com>
2021-08-22 17:11:12 -0400

    feat: add smooth player movement

--- Assets/Developers/Lilou/Scripts/PlayerMovement.cs
+++ Assets/Developers/Lilou/Scripts/PlayerMovement.cs
@@ -8,13 +8,14 @@ public class PlayerMovement : MonoBehaviour
 
     // // physics
     public Rigidbody2D RigidBody;
+    
+    // // input
+    private Vector2 InputAxis;
 
     // // movement
     public float MaxSpeed;
-    public float MaxAcceleration;
-
-    // // movement
-    private Vector2 MovementDirection;
+    public float Acceleration;
+    public float Friction;
 
     // methods
 
@@ -27,25 +28,61 @@ public class PlayerMovement : MonoBehaviour
     // // on frame update
     void Update()
     {
-        ManageInput();
+        UpdateInput();
     }
 
     // // on physics update
     private void FixedUpdate()
     {
-        AddMovementInput(MovementDirection, MaxSpeed);
+        UpdateMovement();
     }
 
-    // // manage input
-    void ManageInput()
+    // // update input
+    void UpdateInput()
     {
-        MovementDirection = new Vector2(-Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        // update input axis
+        InputAxis = GetInputAxis();
     }
 
-    // // add movement input
-    void AddMovementInput(Vector2 Direction, float Delta)
+    // // update movement
+    void UpdateMovement()
+    {
+        if (InputAxis == Vector2.zero)
+        {
+            ApplyFriction(Friction);
+        }
+        else
+        {
+            ApplyMovement(InputAxis, Acceleration);
+        }
+    }
+
+    // // get input axis
+    Vector2 GetInputAxis()
+    {
+        return new Vector2(-Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    // // apply movement
+    void ApplyMovement(Vector2 Direction, float Amount)
     {
         // apply input to rigid body
-        RigidBody.velocity = new Vector2((Direction.x * Delta), (Direction.y * Delta));
+        RigidBody.velocity += Direction * Amount;
+        RigidBody.velocity = Vector2.ClampMagnitude(RigidBody.velocity, MaxSpeed);
+
+        // RigidBody.velocity = new Vector2((Direction.x * Delta), (Direction.y * Delta));
+    }
+
+    // // apply friction
+    void ApplyFriction(float Amount)
+    {
+        if (RigidBody.velocity.magnitude > Amount)
+        {
+            RigidBody.velocity -= RigidBody.velocity.normalized * Amount;
+        }
+        else
+        {
+            RigidBody.velocity = Vector2.zero;
+        }
     }
 }
