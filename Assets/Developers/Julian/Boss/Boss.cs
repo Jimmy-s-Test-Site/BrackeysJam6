1962e7f83e7df2080a05690092da9cfcf7b12eae
jaacko-torus <jaacko.torus@gmail.com>
2021-08-22 16:24:19 -0400

    Boss init

---
+++ Assets/Developers/Julian/Boss/Boss.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+	private void Start()
+	{
+		
+	}
+	
+	private void Update()
+	{
+		
+	}
+}
